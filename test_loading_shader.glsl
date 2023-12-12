fba0c2491d7d03b112212b3569afd1aa67102e26
ammarozkan <61326832+ammarozkan@users.noreply.github.com>
2023-12-12 03:42:45 +0300

    This will be actual UI shader later.

--- test_loading_shader.glsl
+++ test_loading_shader.glsl
@@ -1,7 +1,7 @@
 #GLSL_vertex
 #version 330 core
 layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
-  
+
 out vec3 ourColor; // output a color to the fragment shader
 
 uniform vec2 screen_size;
@@ -12,16 +12,18 @@ uniform mat4 transform;
 
 void main()
 {
-    ui_position = vec2((object_size.x*aPos.x+position.x)/screen_size.x, (object_size.y*aPos.y+position.y)/screen_size.y);
-    gl_Position = transform * vec4(ui_position.xy, 0.0, 1.0);
-    ourColor = vec4(1.0);
+    vec4 aPos_t = transform*vec4(aPos.xy,0.0,1.0);
+    float ui_posx = 2*(object_size.x*aPos_t.x+position.x)/screen_size.x - 1.0;
+    float ui_posy = 2*(object_size.y*aPos_t.y+position.y)/screen_size.y - 1.0;
+    gl_Position = vec4(ui_posx, ui_posy, 0.0, 1.0);
+    ourColor = vec3(1.0);
 }
 
 #GLSL_fragment
 #version 330 core
-out vec4 FragColor;  
+out vec4 FragColor;
 in vec3 ourColor;
-  
+
 void main()
 {
     FragColor = vec4(ourColor, 1.0);
