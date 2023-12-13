4a20c936975e798054f9b1025d6ff8fec51ff66f
ammarozkan <61326832+ammarozkan@users.noreply.github.com>
2023-12-13 03:33:13 +0300

    Yeah yeah. Shaders.
    Should I put here my custom package files? Maybe, for example.

--- uishader.glsl
+++ uishader.glsl
@@ -1,28 +1,30 @@
 #GLSL_vertex
 #version 330 core
-layout (location = 0) in vec2 aPos;
-out vec4 vertexColor;
+layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
 
-uniform vec3 color;
+out vec3 ourColor; // output a color to the fragment shader
 
-uniform mat4 projection;
-uniform mat4 model;
-uniform mat4 view;
+uniform vec2 screen_size;
+uniform vec2 object_size;
+uniform vec2 position;
+
+uniform mat4 transform;
 
 void main()
 {
-	gl_Position = projection * view * model * vec4(aPos.xy,0.0,1.0);
-	vertexColor = vec4(vec3(1),1.0);
+    vec4 aPos_t = transform*vec4(aPos.xy,0.0,1.0);
+    float ui_posx = 2*(object_size.x*aPos_t.x+position.x)/screen_size.x - 1.0;
+    float ui_posy = 2*(object_size.y*aPos_t.y+position.y)/screen_size.y - 1.0;
+    gl_Position = vec4(ui_posx, ui_posy, 0.0, 1.0);
+    ourColor = vec3(1.0);
 }
 
 #GLSL_fragment
 #version 330 core
-
-in vec4 vertexColor;
-
 out vec4 FragColor;
+in vec3 ourColor;
 
 void main()
 {
-	FragColor = vertexColor;
+    FragColor = vec4(ourColor, 1.0);
 }
